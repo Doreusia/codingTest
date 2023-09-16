1a0e79d2c539ac28793dd6a2f3b1c5295863d7f2
Doreusia <shyu6370@gmail.com>
2023-09-16 14:09:39 +0900

    백준 1158번
    백준 26008은 풀지 못함

---
+++ main/src/baekjoon/bj1158/Main.java
@@ -0,0 +1,29 @@
+package baekjoon.bj1158;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int index = 0;
+
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            list.offer(i);
+        }
+
+        System.out.print("<");
+        while (!list.isEmpty()) {
+            index = (index + (k - 1)) % list.size();
+            if(list.size() != 1) {
+                System.out.print(list.remove(index) + ", ");
+            }else{
+                System.out.print(list.remove(index));
+            }
+        }
+        System.out.print(">");
+    }
+}
